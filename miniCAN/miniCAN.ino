64877e2700b30ef9fbe2cfd69e37005693eb7b43
bruno <bruno.marelli@gmail.com>
2018-09-24 20:12:06 +0200

    Check is setup is working.

--- miniCAN/miniCAN.ino
+++ miniCAN/miniCAN.ino
@@ -1,9 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);      // open the serial port at 115200 bps:    
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println("Hello");
 }
