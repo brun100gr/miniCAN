034d5f0177e868027db78193a8e6e0ae9a96e62c
bruno <bruno.marelli@gmail.com>
2018-09-26 23:40:12 +0200

    Button interrupt implementated but not tested.

--- miniCAN/miniCAN.ino
+++ miniCAN/miniCAN.ino
@@ -1,11 +1,26 @@
+const byte interruptPin = 2;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);      // open the serial port at 115200 bps:    
-
-
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), my_interrupt_handler, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello");
+
 }
+
+void my_interrupt_handler()
+{
+ static unsigned long last_interrupt_time = 0;
+ unsigned long interrupt_time = millis();
+ // If interrupts come faster than 200ms, assume it's a bounce and ignore
+ if (interrupt_time - last_interrupt_time > 200) 
+ {
+   Serial.println("Interrupt triggered.");
+ }
+ last_interrupt_time = interrupt_time;
+}
+
