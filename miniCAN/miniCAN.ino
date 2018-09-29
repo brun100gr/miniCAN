aa205b0536b7f151dfe2a622f4924d4754c54254
bruno <bruno.marelli@gmail.com>
2018-09-29 21:51:42 +0200

    Multiple buttons detection with one input implemented and tested.

--- miniCAN/miniCAN.ino
+++ miniCAN/miniCAN.ino
@@ -1,32 +1,30 @@
 int old_button = 0;
-int button;
-int pressed_button;
+int pressed_button = 0;
+//int sensorPin = A1;    // select the input pin for the potentiometer
+#define SENSOR_PIN  A1 // select the input pin for the potentiometer
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);      // open the serial port at 115200 bps:
-  pinMode(A5, INPUT); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int buttonAnalogValue = analogRead(A5);
+  int button = 0;
+  int buttonAnalogValue = analogRead(SENSOR_PIN);
+  // Serial.print("Analog value: "); Serial.println(buttonAnalogValue); // debug log
   if (buttonAnalogValue > 1021) button = 0;
-  else if (buttonAnalogValue > 511 && buttonAnalogValue < 514) button = 1;
-  else if (buttonAnalogValue > 680 && buttonAnalogValue < 684) button = 2;
-  else if (buttonAnalogValue > 766 && buttonAnalogValue < 770) button = 3;
-  else if (buttonAnalogValue > 817 && buttonAnalogValue < 822) button = 4;
-  else if (buttonAnalogValue > 851 && buttonAnalogValue < 856) button = 5;
-  else if (buttonAnalogValue > 875 && buttonAnalogValue < 880) button = 6;
+  else if (buttonAnalogValue > 838 && buttonAnalogValue < 868) button = 1;
+  else if (buttonAnalogValue > 803 && buttonAnalogValue < 833) button = 2;
+  else if (buttonAnalogValue > 752 && buttonAnalogValue < 782) button = 3;
+  else if (buttonAnalogValue > 666 && buttonAnalogValue < 696) button = 4;
+  else if (buttonAnalogValue > 494 && buttonAnalogValue < 524) button = 5;
   else button = 0;
 
-  if (old_button == button) {
-    old_button = button;
-    pressed_button = 0;
-  }else{
+  if (old_button != button) {
     old_button = button;
     pressed_button = button;
+    Serial.println(pressed_button);
   }
-  Serial.println(pressed_button);
 }
 
