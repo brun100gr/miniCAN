72e32e5e30d39a6a5057873d0bb88a053c04d5f6
bruno <bruno.marelli@gmail.com>
2018-10-04 13:00:43 +0200

    Button interrupt detection integrated but not tested.

--- miniCAN/miniCAN.ino
+++ miniCAN/miniCAN.ino
@@ -1,14 +1,15 @@
 #define CALIBRATE 0
-#define SENSOR_PIN  A1 // select the input pin for the potentiometer
+#define SENSOR_PIN		A1	// select the input pin for the potentiometer
+#define INTERRUPT_PIN	2	// select the input pin for interrupt
 
 int old_button = 0;
 int pressed_button = 0;
-const byte interruptPin = 2;
+volatile bool buttonPressed = false;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), my_interrupt_handler, FALLING);
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), my_interrupt_handler, FALLING);
 
   Serial.begin(115200);      // open the serial port at 115200 bps:
 }
@@ -75,11 +76,12 @@ void calibrate(int buttonAnalogValue) {
 }
 #endif
 
-void loop() {
-	// put your main code here, to run repeatedly:
+int buttonDetection()
+{
 	int button = 0;
 	int buttonAnalogValue = analogRead(SENSOR_PIN);
 
+	buttonPressed = false;
 #if (CALIBRATE == 1)
 	calibrate(buttonAnalogValue);
 	return;
@@ -100,6 +102,16 @@ void loop() {
 			pressed_button = button;
 			Serial.println(pressed_button);
 		}
+
+		return button;
+	}
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+	if (buttonPressed)
+	{
+		buttonDetection();
 	}
 }
 
@@ -110,7 +122,8 @@ void my_interrupt_handler()
 	// If interrupts come faster than 200ms, assume it's a bounce and ignore
 	if (interrupt_time - last_interrupt_time > 200) 
 	{
-		Serial.println("Interrupt triggered.");
+		Serial.println("Button pressed.");
+		buttonPressed = true;	// use this flag in mainloop to trigger analo reading.
 	}
 	last_interrupt_time = interrupt_time;
 }
